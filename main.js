2d0b0ec99ac7cf4165a25be225b8f5413e5deef2
Khodazone <khodazone@gmail.com>
2023-03-29 17:53:59 +0200

    js end

--- main.js
+++ main.js
@@ -2,9 +2,30 @@
 let mykm = prompt("How many kilometers you want to travel for:");
 let myage = prompt("Insert here your age:");
 
-// conversion type
+// Conversion type
 mykm = parseInt(mykm); 
 myage = parseInt(myage);
+
+// Calculate price
+let pricePerKilometer = 0.21;
+let kilometers = mykm;
+let ticketPrice = pricePerKilometer * mykm;
+let priceDiscount20 = ticketPrice / 100 * 80;
+let priceDiscount40 = ticketPrice / 100 * 60;
+
+// Apply discount based on the age
+if (myage < 18){
+    console.log("Total price with a 20% discount:" + priceDiscount20);
+    document.getElementById("20%discount").innerHTML = priceDiscount20.toFixed(2)+"€;";
+}
+else if (myage > 65){
+    console.log("Total price with a 40% discount:" + priceDiscount40);
+    document.getElementById("40%discount").innerHTML = priceDiscount40.toFixed(2)+"€;";
+}
+else{
+    console.log("Total price without a discount:" + ticketPrice);
+    document.getElementById("Ticketprice").innerHTML = ticketPrice.toFixed(2)+"€;";
+}
 
 // Print information output
 let userData = `
